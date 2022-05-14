50638a1f5e73ddd8cad0f09ca85f4c3154448bba
ivan-melehin <104354349+ivan-melehin@users.noreply.github.com>
2022-05-14 20:43:16 +0300

    Method-Signature

---
+++ src/exercise/App.java
@@ -0,0 +1,19 @@
+package exercise;
+
+public class App {
+    public static String getHiddenCard(String cardNumber, int starsCount) {//метод задаёт параметры метода: стороку cardNumber т.е. номер карты и число, количество звёздочек
+        var visibleDigitsLine = cardNumber.substring(12);// создаём переменную в которой к номеру карты применяется метод который скрывает первые 12 символов номера(номер состоит из 16 символов)
+        return "*".repeat(starsCount) + visibleDigitsLine;//к символу звёздочке применяется метод повторения repeat с числом из параметра основного метода starsCount и плюсуется cardNumber уже со скрытыми числами в виде переменной visibleDigitsLine
+    }
+
+    public static String getHiddenCard(String cardNumber) {
+        return App.getHiddenCard(cardNumber, 4);//создам такой же метод, только с одним параметром стринг и возвращаем метод уже с настроеными пераметрами
+    }
+
+    public static void main(String[] args) {
+        var cadrTwo  = "9876543219876777";
+        var card =  App.getHiddenCard(cadrTwo,5);//создаём переменную с методом и вноси данные в пераметры метода
+        System.out.println(card);//выводим метод через переменную
+    }
+
+}
